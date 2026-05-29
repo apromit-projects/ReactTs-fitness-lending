b772dce1c76eb20d565e4517317be3808ccfd23c
apromitXiaomi <realapromit@gmail.com>
2026-05-29 23:00:54 +0300

    [App,types,utils,components] Added sections home, benefits

---
+++ src/utils/Heading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Heading = ({ children }: Props) => {
+  return (
+    <h2 className="basis-3/5 font-montserrat text-3xl font-bold">{children}</h2>
+  );
+};
+
+export default Heading;
---
+++ src/utils/benefitData.tsx
@@ -0,0 +1,23 @@
+import { type BenefitType } from "@/types/type";
+import { HiHomeModern, HiMiniUserGroup, HiAcademicCap } from "react-icons/hi2";
+
+export const benefits: Array<BenefitType> = [
+  {
+    icon: <HiHomeModern className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <HiMiniUserGroup className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <HiAcademicCap className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
